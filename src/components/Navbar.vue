758e3630c6526e40060266e28567d0376d33ac03
Willian Mikhael da Cunha <willian_mikhael@outlook.com>
2023-04-02 15:59:16 -0300

    add banner

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,48 @@
 <template>
-    <div id="nav">
-        <router-link to="/">
-            <img src="" id="logo">
-        </router-link>
-    <router-link to="/">Home</router-link> |
+  <div id="nav">
+      <router-link to="/" id="logo-url">
+        <img :src="logo" :alt="alt" id="logo" />
+      </router-link>
+      <router-link to="/">Home</router-link>
     <router-link to="/pedidos">Pedidos</router-link>
   </div>
 </template>
+
 <script>
 export default {
-    name: "Navbar"
-}
+  name: "Navbar",
+  props: ["logo", "alt"],
+};
 </script>
-<style scoped>
 
+<style scoped>
+#nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+#logo-url {
+  margin: auto !important;
+  margin-left: 0 !important;
+}
+
+#logo {
+  width: 40px;
+  height: 40px;
+}
+
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  color: #fff;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
+ <banner />
+  <div class="main-container">
+    <h1>Monte o seu Burguer:</h1>
+    <p>formulario</p>
   </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue'
 
+
+export default {
+  name: "Home",
+  components: {
+    Banner
+  }
+}
 </script>
