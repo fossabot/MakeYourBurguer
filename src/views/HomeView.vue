524481ba336bdc1e3a6f281c7f61191841e8b64e
Willian Mikhael da Cunha <willian_mikhael@outlook.com>
2023-04-02 16:39:53 -0300

    add form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,20 @@
  <banner />
   <div class="main-container">
     <h1>Monte o seu Burguer:</h1>
-    <p>formulario</p>
+    <burgerform />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue'
+import Burgerform from '@/components/Burgerform.vue'
 
 
 export default {
   name: "Home",
   components: {
-    Banner
+    Banner,
+    Burgerform,
   }
 }
 </script>
